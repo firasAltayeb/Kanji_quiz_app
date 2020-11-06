d827add691c9b0c10140ebe45e8fa11e56329357
Firas Altayeb <tayebfiras@gmail.com>
2020-11-06 23:55:15 +0900

    Moved files into folders

--- lib/widgets/quiz.dart
+++ lib/widgets/quiz.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import 'widgets/question.dart';
+import 'widgets/answer.dart';
 
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
