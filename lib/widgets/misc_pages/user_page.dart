40eef9b439ab3612cfca64b00c3fec875c30cfb7
Firas Altayeb <tayebfiras@gmail.com>
2020-12-12 23:24:04 +0900

    added a few inital widgets for the user page

--- lib/widgets/misc_pages/user_page.dart
+++ lib/widgets/misc_pages/user_page.dart
@@ -9,14 +9,41 @@ class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('User Page'),
-          backgroundColor: Colors.black,
-        ),
-        body: Column(
-          children: [
-            Text('hi'),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text('User Page'),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search_rounded,
+              color: Colors.white,
+              size: MediaQuery.of(context).size.height * 0.05,
+            ),
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Icon(
+            Icons.account_circle_rounded,
+            color: Colors.black,
+            size: MediaQuery.of(context).size.height * 0.3,
+          ),
+          Text(
+            "James Jackson",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
   }
 }
