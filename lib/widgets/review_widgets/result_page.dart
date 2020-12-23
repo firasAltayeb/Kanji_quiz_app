f48ea720b57491fd8fd463789573ab03b704a5ab
Firas Altayeb <tayebfiras@gmail.com>
2020-12-23 23:38:00 +0900

    improved result page kanji rows look

--- lib/widgets/review_widgets/result_page.dart
+++ lib/widgets/review_widgets/result_page.dart
@@ -1,4 +1,3 @@
-import 'package:Kanji_quiz_app/widgets/misc_widgets/kanji_block_row.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -20,51 +19,58 @@ class ResultPage extends StatelessWidget {
     double spaceHeight = MediaQuery.of(context).size.height;
 
     final children = <Widget>[
-      SizedBox(height: spaceHeight * 0.05),
-      wrapUpButton(spaceWidth),
-      SizedBox(height: spaceHeight * 0.05),
+      SizedBox(height: spaceHeight * 0.03),
+      wrapUpButton(spaceWidth * 0.6),
+      SizedBox(height: spaceHeight * 0.03),
       Text(
         'Your session score is ${scoreToDisplay.toString()}',
         style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
       ),
-      SizedBox(height: spaceHeight * 0.05),
+      SizedBox(height: spaceHeight * 0.03),
       textContainer('Recalled Correctly', Colors.green),
     ];
 
-    updateChildren(children, correctRecallList, spaceHeight, spaceWidth);
+    kanjiChildrenRow(children, correctRecallList, spaceHeight);
     children.add(textContainer('Recalled Incorrectly', Colors.red));
-    updateChildren(children, incorrectRecallList, spaceHeight, spaceWidth);
+    kanjiChildrenRow(children, incorrectRecallList, spaceHeight);
 
-    return SingleChildScrollView(
-      child: Column(children: children),
-    );
+    return Column(children: children);
   }
 
-  void updateChildren(
-      List<Widget> children, List<String> list, double schigt, double sWdth) {
-    for (int index = 0; index <= list.length; index++) {
-      var reminder = index % 4;
-      if (index > 0 && reminder == 0) {
-        children.add(
-            kanjiRowContainer(schigt, sWdth, list.sublist(index - 4, index)));
-      } else if (index == list.length) {
-        if (index % 4 == 3) {
-          children.add(kanjiRowContainer(
-              schigt, sWdth * 0.7, list.sublist((index - reminder), index)));
-        } else if (index % 4 == 2) {
-          children.add(kanjiRowContainer(
-              schigt, sWdth * 0.5, list.sublist((index - reminder), index)));
-        } else if (index % 4 == 1) {
-          children.add(kanjiRowContainer(
-              schigt, sWdth * 0.25, list.sublist((index - reminder), index)));
-        }
-      }
-    }
+  void kanjiChildrenRow(
+      List<Widget> children, List<String> list, double height) {
+    children.add(
+      list.length == 0
+          ? SizedBox()
+          : SizedBox(
+              width: double.infinity,
+              height: height * 0.25,
+              child: GridView(
+                children: (list)
+                    .map(
+                      (blockAddress) => Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(blockAddress),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 1.25,
+                ),
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+    );
   }
 
   Widget wrapUpButton(double spaceWidth) {
     return ButtonTheme(
-      minWidth: spaceWidth * 0.6,
+      minWidth: spaceWidth,
       splashColor: Colors.green,
       child: RaisedButton(
         child: Text(
@@ -102,17 +108,6 @@ class ResultPage extends StatelessWidget {
           fontWeight: FontWeight.bold,
           color: Colors.white,
         ),
-      ),
-    );
-  }
-
-  Widget kanjiRowContainer(double schigt, double sWdth, List<String> list) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Container(
-        width: sWdth,
-        height: schigt * 0.19,
-        child: KanjiBlockRow(list),
       ),
     );
   }
