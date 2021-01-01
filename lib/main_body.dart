e1e0f0ebd17d86a3bcc539d5fc1fcb9571d4000e
Firas Altayeb <tayebfiras@gmail.com>
2021-01-01 11:24:32 +0900

    made class names make more since

--- lib/main_body.dart
+++ lib/main_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'widgets/lesson_widgets/lesson_manager.dart';
-import 'widgets/review_widgets/review_manager.dart';
+import 'widgets/lesson_widgets/lesson_mgr_screen.dart';
+import 'widgets/review_widgets/review_mgr_screen.dart';
 
 class MainBody extends StatelessWidget {
   final Function reAllocateMaps;
