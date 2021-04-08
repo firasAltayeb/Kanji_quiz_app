3bbe3844b74bb7429e06f4855854b5966dda6b55
Firas Altayeb <tayebfiras@gmail.com>
2021-04-09 04:31:58 +0900

    moved kanji fixed data from json to kanji data object

--- lib/model/kanji_model.dart
+++ lib/model/kanji_model.dart
@@ -3,11 +3,16 @@ import 'package:flutter/foundation.dart';
 class Kanji {
   final String keyword;
   final String itemType;
+  final String jlptLevel;
   final int frameNumber;
   final int koohiiNumber;
   final String characterLook;
   final List<String> buildingBlocksLook;
   final List<String> buildingBlockKeywords;
+  final List<String> similarCharactersLook;
+  final List<String> similarCharactersKeyword;
+  final int frequencyInAozora;
+  final List<String> kanjiMeanings;
 
   int progressLevel;
   String mnemonicStory;
@@ -17,14 +22,19 @@ class Kanji {
   Kanji({
     @required this.keyword,
     @required this.itemType,
+    @required this.jlptLevel,
     @required this.frameNumber,
     @required this.koohiiNumber,
     @required this.characterLook,
     @required this.buildingBlocksLook,
     @required this.buildingBlockKeywords,
+    @required this.similarCharactersLook,
+    @required this.similarCharactersKeyword,
+    @required this.frequencyInAozora,
+    @required this.kanjiMeanings,
     this.progressLevel = 1,
     this.mnemonicStory = "",
     this.learningStatus = "Lesson",
-    this.dateLastLevelChanged,
+    @required this.dateLastLevelChanged,
   });
 }
