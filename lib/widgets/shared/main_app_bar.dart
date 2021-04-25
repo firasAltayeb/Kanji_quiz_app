2dadfd1a910852043e52544ca56ed0a5e6633c71
Firas Altayeb <tayebfiras@gmail.com>
2021-04-26 00:14:58 +0900

    added a vert popup menu button

--- lib/widgets/shared/main_app_bar.dart
+++ lib/widgets/shared/main_app_bar.dart
@@ -1,23 +1,42 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kanji_quiz_app/model/kanji_model.dart';
+import 'package:kanji_quiz_app/helper_functions.dart';
+import 'package:kanji_quiz_app/main_providers.dart';
+import '../../screens/user_page_screen.dart';
 import 'package:flutter/material.dart';
 
-import '../../screens/user_page_screen.dart';
+enum VertOptions { User, WrapUp, Koohii }
 
-class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final String title;
+class MainAppBar extends ConsumerWidget implements PreferredSizeWidget {
   final AppBar appBar;
+  final String passedTitle;
 
-  MainAppBar({
-    Key key,
-    this.title,
-    this.appBar,
-  }) : super(key: key);
+  MainAppBar({Key key, this.passedTitle, this.appBar}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    var screenHeight = MediaQuery.of(context).size.height;
+  void _choiceAction(
+    VertOptions choice,
+    BuildContext context,
+    Kanji targetKanji,
+    List<bool> ansChoiceList,
+    List<Kanji> reviewList,
+  ) {
+    if (choice == VertOptions.User) {
+      Navigator.of(context).pushNamed(UserPage.routeName);
+    } else if (choice == VertOptions.Koohii) {
+      launchURL(targetKanji);
+    } else if (choice == VertOptions.WrapUp) {
+      wrapSession(context, ansChoiceList, reviewList);
+    }
+  }
+
+  Widget build(BuildContext context, ScopedReader watch) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final targetKanji = watch(targetKanjiProvider).state;
+    final ansChoiceList = watch(answerChoiceListProvider).state;
+    final reviewList = watch(reviewReadyListProvider);
     return AppBar(
       title: Text(
-        title,
+        passedTitle + "Page",
         style: TextStyle(
           fontSize: screenHeight * 0.03,
           fontFamily: 'Anton',
@@ -32,16 +51,34 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
           ),
           onPressed: null,
         ),
-        IconButton(
-          icon: Icon(
-            Icons.account_circle_outlined,
-            color: Colors.white,
-            size: screenHeight * 0.035,
-          ),
-          onPressed: () => Navigator.push(
+        PopupMenuButton(
+          onSelected: (choice) => _choiceAction(
+            choice,
             context,
-            MaterialPageRoute(builder: (context) => UserPage()),
+            targetKanji,
+            ansChoiceList,
+            reviewList,
           ),
+          icon: Icon(
+            Icons.more_vert,
+          ),
+          itemBuilder: (_) => [
+            if (passedTitle == "Home")
+              PopupMenuItem(
+                value: VertOptions.User,
+                child: Text('User page'),
+              ),
+            if (passedTitle == "Review")
+              PopupMenuItem(
+                value: VertOptions.WrapUp,
+                child: Text('Wrap up session'),
+              ),
+            if (passedTitle == "Lesson" || passedTitle == "Item Detail")
+              PopupMenuItem(
+                value: VertOptions.Koohii,
+                child: Text('Search in Koohii'),
+              ),
+          ],
         ),
       ],
     );
